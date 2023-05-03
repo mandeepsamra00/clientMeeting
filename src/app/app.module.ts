6badf02b38f82024d3b3e6abf5117118a2d8124c
Mandeep Samra <MandeepSamra@compassmade.com>
2023-05-03 11:33:00 -0700

    Generate Client management component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClientManagementComponent } from './client-management/client-management.component';
+import { ClientMeetingComponent } from './client-meeting/client-meeting.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientManagementComponent,
+    ClientMeetingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/client-meeting/client-meeting.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-meeting',
+  templateUrl: './client-meeting.component.html',
+  styleUrls: ['./client-meeting.component.css']
+})
+export class ClientMeetingComponent {
+
+}
